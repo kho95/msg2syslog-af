43726fc724f488d30bd70f2cae9a3630c0885a15
Eugene Fong <efon103@aucklanduni.ac.nz>
2019-01-29 09:24:58 +1300

    add token refresh and alert feed interval pull functionality

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const querystring = require("querystring");
 
 require('dotenv').config({path: __dirname + '/.env'});
 
-var token, expiry;
+var time_interval = 60000; // polling time interval in millis
+
+var client, token, expiry;
 
 function getToken(callback) {
 
@@ -46,21 +48,40 @@ function getToken(callback) {
     req.end(data);
 }
 
-getToken((c) => {
-    token = c.token;
-    expiry = c.expiry;
-
-    var client = MicrosoftGraph.Client.init({
-        authProvider: (done) => {
-            done(null, token); //first parameter takes an error if you can't get an access token
-        }
-    });
+async function refresh() {
+    return new Promise(function(resolve, reject) {
+        getToken((c) => {
+            token = c.token;
+            expiry = parseInt(c.expiry);
     
-    client.api('/security/alerts').top(1000).get((err, res) => {
+            client = MicrosoftGraph.Client.init({
+                authProvider: (done) => {
+                    done(null, token); //first parameter takes an error if you can't get an access token
+                }
+            });
+    
+            setTimeout(function(){ refresh() }, ( Date.now + parseInt(c.expiry) ));
+            resolve(token)
+        });      
+    });
+};
+
+function getAlerts() {
+    client.api('/security/alerts').top(1).get((err, res) => {
         console.log(err); // prints info about authenticated user
         console.log(res); // prints info about authenticated user
     });
-});
+}
 
+async function main() {
+    if (token == null || expiry == null || expiry < Date.now){
+        await refresh();
+        getAlerts();
+    }
+    
+    setInterval(function() {
+        getAlerts();
+    }, time_interval);
+}
 
-
+main();
